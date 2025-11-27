8b6409f89babcbdf98c1fa74779b3d8b3274a4e6
CELESTIN RUSHIGIRA DONNIE <celestinrushigiradonnie@gmail.com>
2025-11-27 12:59:54 +0200

    first commit

--- src/main/java/cd/ac/uea/assessment/order/InventoryClient.java
+++ src/main/java/cd/ac/uea/assessment/order/InventoryClient.java
@@ -1,13 +1,14 @@
 package cd.ac.uea.assessment.order;
-
 import org.springframework.stereotype.Component;
 
 @Component
 public class InventoryClient {
 
     public boolean checkStock(Long productId) {
-        // Simulating external call
-        // In a real scenario, this might call another microservice
-        return true; 
+        if (productId == 2L) {
+            return false;
+        }
+
+        return true;
     }
 }
